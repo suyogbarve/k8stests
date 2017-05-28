b9712369297f5e7c70310829c087436fb9eceb74
Suyog Barve <Suyog.Barve@walmart.com>
2017-05-28 12:49:05 -0500

    unit test for rollout_status.go

---
+++ rollout_status_test.go
@@ -0,0 +1,154 @@
+/*
+Copyright 2014 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rollout
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/rest/fake"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/v1"
+	extensionsv1beta1 "k8s.io/kubernetes/pkg/apis/extensions/v1beta1"
+	cmdtesting "k8s.io/kubernetes/pkg/kubectl/cmd/testing"
+	"k8s.io/kubernetes/pkg/kubectl/resource"
+)
+
+func defaultHeader() http.Header {
+	header := http.Header{}
+	header.Set("Content-Type", runtime.ContentTypeJSON)
+	return header
+}
+
+func objBody(codec runtime.Codec, obj runtime.Object) io.ReadCloser {
+	return ioutil.NopCloser(bytes.NewReader([]byte(runtime.EncodeOrDie(codec, obj))))
+}
+
+func stringBody(body string) io.ReadCloser {
+	return ioutil.NopCloser(bytes.NewReader([]byte(body)))
+}
+
+type testVisitor struct {
+	InjectErr error
+	Infos     []*resource.Info
+}
+
+func (v *testVisitor) Handle(info *resource.Info, err error) error {
+	if err != nil {
+		return err
+	}
+	v.Infos = append(v.Infos, info)
+	return v.InjectErr
+}
+
+func (v *testVisitor) Objects() []runtime.Object {
+	objects := []runtime.Object{}
+	for i := range v.Infos {
+		objects = append(objects, v.Infos[i].Object)
+	}
+	return objects
+}
+
+type testPrinter struct {
+	Objects []runtime.Object
+	Err     error
+}
+
+func (t *testPrinter) PrintObj(obj runtime.Object, out io.Writer) error {
+	t.Objects = append(t.Objects, obj)
+	fmt.Fprintf(out, "%#v", obj)
+	return t.Err
+}
+func (t *testPrinter) HandledResources() []string {
+	return []string{}
+}
+
+func (t *testPrinter) AfterPrint(output io.Writer, res string) error {
+	return nil
+}
+func TestRunHistory(t *testing.T) {
+	one := int32(1)
+	deploymentObject := &extensionsv1beta1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "extensions/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            "foo",
+			Labels:          map[string]string{"app": "foo"},
+			Namespace:       "default",
+			ResourceVersion: "12345",
+		},
+		Spec: extensionsv1beta1.DeploymentSpec{
+			Replicas: &one,
+			Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"app": "foo"}},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"app": "foo"},
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{{Name: "app", Image: "abc/app:v4"},
+						{Name: "ape", Image: "zyx/ape"}},
+				},
+			},
+		},
+	}
+
+	f, tf, _, ns := cmdtesting.NewAPIFactory()
+	_, _, codec, _ := cmdtesting.NewTestFactory()
+	codec = unstructured.UnstructuredJSONScheme
+	unstructured.
+		tf.Printer = &testPrinter{}
+	tf.Namespace = "default"
+
+	tf.ClientConfig = &restclient.Config{ContentConfig: restclient.ContentConfig{GroupVersion: &schema.GroupVersion{Group: "extensions/v1beta1", Version: "v1beta1"}}}
+	buf := bytes.NewBuffer([]byte{})
+	//tf.UnstructuredClient = &fake.RESTClient{
+	tf.Client = &fake.RESTClient{
+		APIRegistry:          api.Registry,
+		NegotiatedSerializer: ns,
+		Client: fake.CreateHTTPClient(func(req *http.Request) (*http.Response, error) {
+			fmt.Println(req)
+			fmt.Println("********")
+			switch p, m := req.URL.Path, req.Method; {
+			case p == "/namespaces/default/deployments/foo" && m == "GET":
+				return &http.Response{StatusCode: 200, Header: defaultHeader(), Body: objBody(codec, deploymentObject)}, nil
+			default:
+				t.Fatalf("unexpected request: %#v\n%#v", req.URL, req)
+				return nil, nil
+			}
+		}),
+	}
+	cmd := NewCmdRolloutStatus(f, buf)
+	cmd.Flags().Set("revision", "0")
+	cmd.Run(cmd, []string{"deployment", deploymentObject.Name})
+	expectedOutput := "deployment/" + deploymentObject.Name + "\n"
+	if buf.String() != expectedOutput {
+		t.Errorf("expected output: %s, but got: %s", expectedOutput, buf.String())
+	}
+}
+
