85039cc81b60747a0c29e40030d533af5b632a44
Suyog Barve <Suyog.Barve@walmart.com>
2017-05-28 19:38:12 -0500

    added imports

--- get_deployment_test.go
+++ get_deployment_test.go
@@ -1,4 +1,36 @@
+/*
+Copyright 2014 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package cmd
+
+import (
+	"bytes"
+	"net/http"
+	"testing"
+
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/pkg/api/v1"
+	extensionsv1beta1 "k8s.io/client-go/pkg/apis/extensions/v1beta1"
+	"k8s.io/client-go/rest/fake"
+	"k8s.io/kubernetes/pkg/api"
+	cmdtesting "k8s.io/kubernetes/pkg/kubectl/cmd/testing"
+)
 
 func testv1beta1Data() *extensionsv1beta1.Deployment {
 	one := int32(1)
@@ -55,3 +87,4 @@ func TestV1beta1GetObjects(t *testing.T) {
 		t.Errorf("unexpected empty output")
 	}
 }
+
